3af2b8095ce32061cb7015779efed77be70d289f
nicsylvia <nicsylvia15f@gmail.com>
2023-01-18 13:12:28 -0800

    saved workspace

---
+++ Sylvia-MicrosoftTodo/Frontend/NewApp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+// import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
